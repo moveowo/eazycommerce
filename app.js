d1ae851de0275848fb752f2f30a1fd235c0ba6b3
Oluwatimilehin Alabi <alabialvin@gmail.com>
2023-08-23 23:48:34 +0100

    initial cleanup

--- app.js
+++ app.js
@@ -1,17 +1,23 @@
-process.env = require('./sample.env.js')(process.env.NODE_ENV || 'development');
+// Import required modules
+require("dotenv").config();
+const express = require("express");
+const indexRoutes = require("./routes/index.js");
+
+// Set up Express app
+const app = express();
 const port = process.env.PORT || 9000;
-const express = require('express');
 
-let indexRoutes = require('./routes/index.js');
+// Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
-const main = async () => {
-    const app = express();
-    app.use(express.json());
-    app.use(express.urlencoded({ extended: false }));
-    app.use('/', indexRoutes);
-    app.use('*', (req, res) => res.status(404).send('404 Not Found'));
-    app.listen(port, () =>
-        console.log(`App now running and listening on port ${port}`)
-    );
-};
-main();
+// Routes
+app.use("/", indexRoutes);
+
+// 404 Route
+app.use("*", (req, res) => res.status(404).send("404 Not Found"));
+
+// Start the server
+app.listen(port, () => {
+	console.log(`App now running and listening on port ${port}`);
+});
